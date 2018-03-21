e3dff379ecc8d39adab49624f1be3be646311e9d
Jung Krisztian <krisz027@hotmail.com>
2018-03-21 18:56:51 +0100

    Initial commit

---
+++ src/client/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Component is on</h1>
+        );
+    }
+}
+export default App;
